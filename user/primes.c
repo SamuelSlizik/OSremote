77d6900a810d53bffce3818ab7d02ce29fe7d5ff
Samuel Slizik <samo.slizik@gmail.com>
2024-10-06 14:58:38 +0200

    everything but primes run out of fd even tho all redundant fds are being closed

--- user/primes.c
+++ user/primes.c
@@ -11,7 +11,7 @@ main(int argc, char *argv[])
     bool main = true;
     int childPipe[2];
     int parentPipe[2];
-    int buffer[2];
+    int buffer;
     int number = -1;
 
     if (pipe(childPipe) == -1) {
@@ -34,21 +34,25 @@ main(int argc, char *argv[])
             } else {
                 close(childPipe[1]);
                 parentPipe[0] = childPipe[0];
+                childPipe[0] = 0;
                 parentPipe[1] = 0;
             }
         }
 
-        read(parentPipe[0], buffer, sizeof(number) + 1);
+        read(parentPipe[0], &buffer, sizeof(number));
 
-        if (buffer[0] == -1) {
+        if (buffer == -1) {
             if (pid > 0) {
-                write(childPipe[1], &number, sizeof(number) + 1);
+                write(childPipe[1], &buffer, sizeof(number));
                 wait(NULL);
                 close(childPipe[1]);
             }
             close(parentPipe[0]);
             exit(0);
-        } else if (buffer[0] % number != 0) {
+        } else if (number == -1) {
+            number = buffer;
+            printf("prime: %d\n", number);
+        } else if (buffer % number != 0) {
             if (pid == 0) {
                 if (pipe(childPipe) == -1) {
                     printf("Pipe error\n");
@@ -62,26 +66,27 @@ main(int argc, char *argv[])
                     exit(1);
                 } else if (pid > 0) {
                     close(childPipe[0]);
-                    write(childPipe[1], &number, sizeof(number) + 1);
+                    write(childPipe[1], &buffer, sizeof(number));
                 } else {
                     close(childPipe[1]);
+                    close(parentPipe[0]);
                     parentPipe[0] = childPipe[0];
+                    childPipe[0] = 0;
                     parentPipe[1] = 0;
-                    number = buffer[0];
-                    printf("prime: %d\n", number);
+                    number = -1;
                 }
             } else {
-                write(childPipe[1], &number, sizeof(number) + 1);
+                write(childPipe[1], &buffer, sizeof(number));
             }
         }
     }
 
     if (main) {
         for (number = 2; number <= 280; number += 1) {
-            write(childPipe[1], &number, sizeof(number) + 1);
+            write(childPipe[1], &number, sizeof(number));
         }
         number = -1;
-        write(childPipe[1], &number, sizeof(number) + 1);
+        write(childPipe[1], &number, sizeof(number));
         wait(NULL);
     } else {
         close(parentPipe[0]);
