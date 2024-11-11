274ba415935b12448d3e147f31029b771393b2d4
Samuel Slizik <samo.slizik@gmail.com>
2024-11-11 10:08:56 +0100

    i dunno

--- kernel/proc.h
+++ kernel/proc.h
@@ -91,6 +91,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  int alarm_ticks;
   int alarm_interval;
   uint64 alarm_handler;
 
